ca199478746d6f8f83c32828347bdcf91517823e
lian <lian@seafile.com>
2021-04-10 17:19:27 +0800

    update

--- students-fe/src/App.js
+++ students-fe/src/App.js
@@ -25,14 +25,12 @@
 //export default App;
 
 import React, { Component, Fragment } from "react";
-import Header from "./components/Header";
 import Home from "./components/Home";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Header />
         <Home />
       </Fragment>
     );
